3fd1cc2ae07b64d8e71442c9fff30154a1ca3272
Sheetal Kashyap <kashyapsheetal2562@gmail.com>
2026-09-13 15:02:16 +0530

    Update script.js
    exchange

--- frontend/script.js
+++ frontend/script.js
@@ -13,9 +13,14 @@ document.addEventListener("DOMContentLoaded", () => {
        CONFIGURATION
     ============================================================ */
 
-    const BACKEND_URL = "http://127.0.0.1:8000";
-    const API_URL = `${BACKEND_URL}/analyze`;
-    const CONFIDENCE_THRESHOLD = 80;
+   const BACKEND_URL =
+    (window.location.hostname === "localhost" ||
+     window.location.hostname === "127.0.0.1")
+        ? "http://127.0.0.1:8000"
+        : window.location.origin;
+
+const API_URL = `${BACKEND_URL}/analyze`;
+const CONFIDENCE_THRESHOLD = 80;
 
     /* ============================================================
        APPLICATION STATE
